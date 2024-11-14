af4848d6844bbd48e18a66c11e7dae85f6741cb0
Hells <96310786+He11Cut3@users.noreply.github.com>
2024-11-14 16:30:47 +0300

    v2

--- X-Tech_TestWork(1)/obj/Debug/net8.0-windows/App.g.i.cs
+++ X-Tech_TestWork(1)/obj/Debug/net8.0-windows/App.g.i.cs
@@ -62,7 +62,7 @@ namespace X_Tech_TestWork_1_ {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/X-Tech_TestWork(1);V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/X-Tech_TestWork(1);component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
